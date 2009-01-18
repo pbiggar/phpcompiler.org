13412093874420d4ebaf6d4e993b3f7b4c977bd7
edskodevries <edskodevries@bbcf20a9-8526-0410-90e2-9b3d3f589b1d>
2009-01-18 16:29:19 +0000

    Completely reimplement lastsvnlog.php from scratch. The old version tried to interpret the textual output from svn, which was a bit hacky. Moreover, it was rather difficult to extend. The new version uses the XML output from svn so that parsing is much more robust, and is generally much more maintainable.
    As far as functionality is concerned, the output now shows which branch the log is applicable too, and shows 7 (rather than 3) logs.

--- www/trunk/src/lastsvnlog.php
+++ www/trunk/src/lastsvnlog.php
@@ -1,63 +1,283 @@
 <?php
+	error_reporting(E_STRICT);
+	date_default_timezone_set('Europe/Dublin');
 
-	// Get the most recent log
-	$mostRecentLog = prettify_log(get_svn_log("HEAD", $revMostRecent)); 
-	$mostRecent_1  = prettify_log(get_svn_log($revMostRecent - 1, $dummy));
-	$mostRecent_2  = prettify_log(get_svn_log($revMostRecent - 2, $dummy));
+	/*
+	 * Configuration
+	 */
 
-	$lastWeek = date("Y-m-d", time() - (7 * 24 * 60 * 60));
-	$lastWeekLog = get_svn_log("{" . $lastWeek . "}", $revLastWeek);
+	$CONFIG[svn]          = "/usr/bin/svn";
+	$CONFIG[repo]         = "http://phc.googlecode.com/svn";
+	$CONFIG[svnExtraArgs] = "--config-dir=/var/empty";
+	$CONFIG[showRevNum]   = 7;
 
-	$numRevisions[] = "<p>There were " . ($revMostRecent - $revLastWeek) . " commits in the last 7 days. Most recent:</p>";
-	$output = array_merge($numRevisions, $mostRecentLog, $mostRecent_1, $mostRecent_2);
-	echo_array($output);
+	/*
+	 * Definition of an svn log
+	 */
 
-	
-	function echo_array($arr)
+	class SvnLog
 	{
-		foreach($arr as $line) echo ("$line\n");
+		public $entries;
+
+		function SvnLog()
+		{
+			$this->entries = array();
+		}
+
+		function prettyHTML($out)
+		{
+			foreach(array_reverse($this->entries) as $entry)
+			{
+				fwrite($out, "<p>\n");
+				$entry->prettyHTML($out);
+				fwrite($out, "</p>\n");
+			}
+		}
+
+		function mostRecent()
+		{
+			// We assume that svn returns the logs in ascending order
+			return end($this->entries)->revision;
+		}
 	}
 
-	function get_svn_log($revision, &$revNo)
+	class SvnLogEntry
 	{
-		exec("/usr/bin/svn --config-dir=/var/empty log -r $revision http://phc.googlecode.com/svn", $log, $return);
+		public $revision;
+		public $author = NULL;
+		public $date = NULL;
+		public $paths = NULL;
+		public $msg = NULL;
+
+		function SvnLogEntry($revision)
+		{
+			$this->revision = $revision;
+		}
+
+		function prettyHTML($out)
+		{
+			$branch = $this->getBranch();
+	
+			/*
+			 * Date format used by svn (http://www.w3.org/TR/NOTE-datetime)
+			 * 
+			 *   YYYY-MM-DDThh:mm:ss.sTZD
+			 *
+			 * where
+			 *
+			 * YYYY = four-digit year
+			 * MM   = two-digit month (01=January, etc.)
+			 * DD   = two-digit day of month (01 through 31)
+			 * hh   = two digits of hour (00 through 23) (am/pm NOT allowed)
+			 * mm   = two digits of minute (00 through 59)
+			 * ss   = two digits of second (00 through 59)
+			 * s    = one or more digits representing a decimal fraction of a second
+			 * TZD  = time zone designator (Z or +hh:mm or -hh:mm)
+       *
+			 * Unfortunately, this date cannot be parsed automatically by PHP4 
+			 * so we do it manually.
+			 */
+
+			preg_match('/^(.*)T(.*)\..*$/', $this->date, $bits);
+			list($_, $date, $time) = $bits;
+
+			/*
+			 * Highlight string inserts non-breaking spaces in the "HTML" part of the message,
+			 * which makes it impossible for the browser to break lines. Remove these.
+			 */
+
+			$highlighted = highlight_string($this->msg, True);
+			$highlighted = str_replace("&nbsp;", " ", $highlighted);
+
+			fwrite($out, "<b>r$this->revision</b> | <b>$this->author</b> | <b>$date $time</b> | <b>$branch</b>\n");
+			fwrite($out, "<br><br>\n");
+			fwrite($out, $highlighted);
+		}
+
+		function getBranch()
+		{
+			if($this->paths === NULL)
+				return "unknown branch";
+
+			$branches = array();
+			foreach($this->paths as $path)
+			{
+				assert(substr($path->filename, 0, 1) === "/");
+				$bits = explode("/", substr($path->filename, 1));
+		
+				if(count($bits) == 1)
+				{
+					$branches[] = $bits[0];
+				}
+				else
+				{
+					assert(count($bits) > 1);
+
+					switch($bits[0])
+					{
+						case "trunk":
+							$branches[] = "trunk";
+							break;
+						case "branches":
+							$branches[] = $bits[1];
+							break;
+						case "tags":
+							$branches[] = $bits[1];
+							break;
+						default:
+							// In the other (unknown) cases, we return the first part of the path
+							// echo "Warning: unknown branch \"$path->filename\"\n";
+							$branches[] = $bits[0];
+							break;
+					}
+				}
+			}
+
+			return implode(", ", array_unique($branches));
+		}
+	}
+
+	class SvnPath
+	{
+		public $action;
+		public $filename = NULL;
+
+		function SvnPath($action)
+		{
+			$this->action = $action;
+		}
+	}
+
+	/*
+	 * Interact with svn
+	 */
+
+	function getSvnLog($revs)
+	{
+		global $CONFIG;
+
+		exec("$CONFIG[svn] $CONFIG[svnExtraArgs] --xml -v log -r $revs $CONFIG[repo]", $log, $return);
 		if($return != 0)
 			die("Could not run <tt>svn</tt>");
 
-		// Extract revision number
-		$dateLine = $log[1];
-		$bits = explode(" | ", $dateLine);
-		$revNo = substr($bits[0], 1);
-		
-		return $log;
+		$parser = new SvnLogParser();
+		foreach($log as $line)
+		{
+			$parser->parse($line, False);
+		}
+		$parser->parse("", True);
+
+		return $parser->getLog();
 	}
 
-	function prettify_log($log)
+	class SvnLogParser
 	{
-		// Remove the ---- lines
-		unset($log[0]);
-		array_pop($log);
+		protected $log = NULL;
+		protected $parser;
+		protected $topElement;
 
-		foreach($log as $key => $line)
+		function SvnLogParser()
 		{
-			$log[$key] = htmlentities($log[$key]);
+			$this->parser = xml_parser_create();
+			xml_set_object($this->parser, $this);
+			xml_set_element_handler($this->parser, "startElementHandler", "endElementHandler");
+			xml_set_character_data_handler($this->parser, "characterDataHandler");
+			xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, 0);
 		}
 
-		// Highlight the date in bold
-		$dateLine = $log[1];
-		$bits = explode(" | ", $dateLine);
-		$lastRevision = substr($bits[0], 1);
-		$bits[2] = "<b>" . $bits[2] . "</b>";
-		$log[1] = "<p>" . implode(" | ", $bits) . "</p><p>";
-		$log[] = "</p>";
-
-		// Replace blank lines by <br>
-		foreach($log as $key => $line)
+		function parse($data, $final)
 		{
-			if($line == "") $log[$key] = "</p><p>";
+			if(!xml_parse($this->parser, $data, $final))
+				die(sprintf("XML error: %s at line %d",
+					xml_error_string(xml_get_error_code($this->parser)),
+					xml_get_current_line_number($this->parser)));
+		}
+		
+		function getLog()
+		{
+			$this->parse("", True);
+			return $this->log;
 		}
 
-		return $log;	
+		function startElementHandler($parser, $name, $attrs)
+		{
+			$this->topElement = $name;
+
+			switch($name)
+			{
+				case "log":
+					assert($this->log === NULL);
+					$this->log = new SvnLog();
+					break;
+				case "logentry":
+					array_push($this->log->entries, new SvnLogEntry($attrs["revision"]));
+					break;
+				case "paths":
+					$entry = end($this->log->entries);
+					assert($entry->paths === NULL);
+					$entry->paths = array();
+					break;
+				case "path":
+					$paths =& end($this->log->entries)->paths;
+					array_push($paths, new SvnPath($attrs["action"]));
+					break;
+				default:
+					// Unknown tag. Ignore for future compatability
+					break;
+			}
+		}
+
+		function endElementHandler($parser, $name)
+		{
+			$this->topElement = NULL;
+		}
+
+		function characterDataHandler($parser, $data)
+		{
+			switch($this->topElement)
+			{
+				case "author":
+					$entry = end($this->log->entries);
+					$entry->author .= $data;
+					break;
+				case "date":
+					$entry = end($this->log->entries);
+					$entry->date .= $data;
+					break;
+				case "path":
+					$entry = end($this->log->entries);
+					$path  = end($entry->paths);
+					$path->filename .= $data;
+					break;
+				case "msg":
+					$entry = end($this->log->entries);
+					$entry->msg .= $data;
+					break;
+				default:
+					// Unknown top element. Ignore for future compatability
+					break;
+			}
+		}
 	}
 
+	/*
+	 * Main
+	 */
+	
+	$lastWeek    = date("Y-m-d", time() - (7 * 24 * 60 * 60));
+
+	$head        = getSvnLog("HEAD");
+	$lastWeek    = getSvnLog("{" . $lastWeek . "}");
+
+	$headRev     = $head->mostRecent();
+	$lastWeekRev = $lastWeek->mostRecent();
+	$headRev_    = ($headRev + 1 - $CONFIG[showRevNum] > 0) 
+										? ($headRev + 1 - $CONFIG[showRevNum]) 
+										: 1;
+
+	$recent      = getSvnLog("$headRev_:$headRev");
+
+	echo "<p>There were " . ($head->mostRecent() - $lastWeek->mostRecent()) . " commits in the last 7 days. Most recent:</p>";
+	$recent->prettyHTML(STDOUT);
 ?>
+<?php // vi:set noexpandtab: ?>
